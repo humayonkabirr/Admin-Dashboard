f30cf6b0f367ce17c414a75b8e2989575ceae060
humayonkabirr <humayonkobir8@gmail.com>
2024-01-14 09:27:44 +0600

    sada;lk

--- resources/js/Pages/SuperAdmin/Dashboard.vue
+++ resources/js/Pages/SuperAdmin/Dashboard.vue
@@ -43,7 +43,7 @@ const sideBar = ref(true);
                                         d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 1 1 0-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 0 1-1.44-4.282m3.102.069a18.03 18.03 0 0 1-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 0 1 8.835 2.535M10.34 6.66a23.847 23.847 0 0 0 8.835-2.535m0 0A23.74 23.74 0 0 0 18.795 3m.38 1.125a23.91 23.91 0 0 1 1.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 0 0 1.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 0 1 0 3.46" />
                                 </svg>
 
-                            </li> 
+                            </li>
                             <li class="cursor-pointer">
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                     stroke="currentColor" class="w-5 h-5">
@@ -148,7 +148,7 @@ const sideBar = ref(true);
                                     <span :class="{ 'hidden': !sideBar }"> Submenu</span>
                                     </Link>
                                 </li>
-                                 
+
                                 <li>
                                     <Link :href="route('dashboard')"
                                         class="flex items-center space-x-1 cursor-pointer text-white p-2 hover:bg-slate-800 duration-300 rounded-md">
@@ -210,7 +210,43 @@ const sideBar = ref(true);
             </div>
 
             <div class="w-full h-[calc(100vh-48px)] mt-12 overflow-y-auto p-3">
-                <div class=" grid grid-cols-1 sm:grid-cols-2 gap-3">
+                <div class=" grid grid-cols-1 sm:grid-cols-2 gap-3 py-2">
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                </div>
+                <div class=" grid grid-cols-2 sm:grid-cols-3 gap-3 py-2">
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                </div>
+                <div class=" grid grid-cols-2 sm:grid-cols-4 gap-3 py-2">
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
+                </div>
+                <div class=" grid grid-cols-2 sm:grid-cols-3 gap-3 py-2">
+                    <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
+                        <h1>Card</h1>
+                    </div>
                     <div class="bg-slate-900 hover:bg-slate-800 duration-1000 h-64 w-full rounded-sm">
                         <h1>Card</h1>
                     </div>
